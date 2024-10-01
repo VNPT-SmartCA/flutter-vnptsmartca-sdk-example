286816592efc2040d55731e8ddf6521a60ea9c6d
tuanbaros <tuanbaros123@gmail.com>
2024-10-01 15:02:28 +0700

    Update demo

---
+++ flutter_partner_app/android/app/src/main/kotlin/com/partner/app/flutter_partner_app/MainActivity.kt
@@ -0,0 +1,116 @@
+package com.partner.app.flutter_partner_app
+
+import android.os.Handler
+import com.vnpt.smartca.*
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity : FlutterFragmentActivity() {
+    var VNPTSmartCA = VNPTSmartCASDK()
+    lateinit var methodChannel: MethodChannel
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+        try {
+            Handler().postDelayed({
+                var customParams = CustomParams(
+                    customerId = "",
+                    customerPhone = "",
+                    borderRadiusBtn = 999.0,
+                    colorSecondBtn = "#DEF7EB",
+                    colorPrimaryBtn = "#33CC80",
+                    featuresLink = "",
+                    packageDefault = "",
+                    // password: "Vnpt@123456"
+                    logoCustom = "",
+                    backgroundLogin = ""
+                );
+
+                  var config = ConfigSDK(
+                    env = SmartCAEnvironment.DEMO_ENV, // Môi trường kết nối DEMO/PROD
+                    clientId = "4185-637127995547330633.apps.signserviceapi.com", // clientId tương ứng với môi trường được cấp qua email
+                    clientSecret = "NGNhMzdmOGE-OGM2Mi00MTg0", // clientSecret tương ứng với môi trường được cấp qua email                    
+                    lang = SmartCALanguage.VI,
+                    isFlutter = true,
+            	    customParams = customParams,
+                )
+
+            VNPTSmartCA.initSDK(this, config)
+
+                methodChannel = MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "com.vnpt.flutter/partner")
+                methodChannel.setMethodCallHandler { call, result ->
+                    when (call.method) {
+                        "createAccount" -> createAccount();
+                        "getAuthentication" -> getAuthentication();
+                        "getMainInfo" -> getMainInfo()
+                        "getWaitingTransaction" -> (call.arguments as? String)?.let { getWaitingTransaction(it) };
+                        "signOut" -> signOut()
+                    }
+                }
+            }, 2000)
+
+        } catch (ex: Exception) {
+            throw ex;
+        }
+    }
+
+    private fun createAccount() {
+        try {
+            VNPTSmartCA.createAccount { result ->
+                methodChannel.invokeMethod("createAccountResult", getMap(result))
+            }
+        } catch (ex: Exception) {
+            throw ex;
+        }
+    }
+
+    private fun getAuthentication() {
+        try {
+            VNPTSmartCA.getAuthentication { result ->
+                methodChannel.invokeMethod("getAuthenticationResult", getMap(result))
+            }
+        } catch (ex: Exception) {
+            throw ex;
+        }
+    }
+
+    private fun getMainInfo() {
+        try {
+            VNPTSmartCA.getMainInfo { result ->
+                methodChannel.invokeMethod("getMainInfoResult", getMap(result))
+            }
+        } catch (ex: Exception) {
+            throw ex;
+        }
+    }
+
+    private fun getWaitingTransaction(transId: String) {
+        try {
+            VNPTSmartCA.getWaitingTransaction(transId,"") { result ->
+                methodChannel.invokeMethod("getWaitingTransactionResult", getMap(result))
+            }
+        } catch (ex: Exception) {
+            throw ex;
+        }
+    }
+
+    private fun signOut() {
+        try {
+            VNPTSmartCA.signOut { result ->
+                methodChannel.invokeMethod("signOutResult", getMap(result))
+            }
+        } catch (ex: Exception) {
+            throw ex;
+        }
+    }
+
+    fun getMap(result: SmartCAResult) : HashMap<String, Any> {
+        var hashMap = HashMap<String, Any> ()
+        hashMap.put("status", result.status)
+        result.statusDesc?.let { hashMap.put("statusDesc", it) }
+        result.data?.let { hashMap.put("data", it) }
+        return hashMap;
+    }
+}
---
+++ flutter_partner_app/ios/Runner/AppDelegate.swift
@@ -0,0 +1,96 @@
+import UIKit
+import Flutter
+import SmartCASDK
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+    
+    var channelFlutter: FlutterMethodChannel?
+    var vnptSmartCASDK: VNPTSmartCASDK?
+    
+    override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        if let rootView = window?.rootViewController as? FlutterViewController {
+            let customParams = CustomParams(
+                customerId: "",
+                borderRadiusBtn: 999,
+                colorSecondBtn: "#FFFFFF",
+                colorPrimaryBtn: "#4788FF",
+                featuresLink: "https://www.google.com/?hl=vi",
+                customerPhone: "",
+                packageDefault: "",
+                password: "",
+                logoCustom: "",
+                backgroundLogin: ""
+            )
+        
+            let config = SDKConfig(
+                clientId: "4c6a-637932338278807354.apps.smartcaapi.com", 
+                clientSecret: "OTk1NjZlYWY-ZWVhMC00YzZh", 
+                environment: ENVIRONMENT.PRODUCTION, 
+                lang: LANG.VI, 
+                isFlutterApp: true, 
+                customParams: customParams
+                );
+
+            self.vnptSmartCASDK = VNPTSmartCASDK(
+                viewController: rootView,
+                config: config)
+            
+            let channel = FlutterMethodChannel(name: "com.vnpt.flutter/partner", binaryMessenger: rootView.binaryMessenger)
+            channel.setMethodCallHandler({ (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+                self.channelFlutter = channel
+                
+                if call.method == "getAuthentication" {
+                    self.getAuthentication()
+                } else if call.method == "getMainInfo" {
+                    self.getMainInfo()
+                } else if call.method == "getWaitingTransaction" {
+                    let transactionId = call.arguments as? String ?? ""
+                    self.getWaitingTransaction(transactionId: transactionId)
+                } else if call.method == "signOut"{
+                    self.signOut()
+                } else if call.method == "createAccount" {
+                    self.createAccount()
+                }
+            })
+        }
+        
+        GeneratedPluginRegistrant.register(with: self)
+        GeneratedPluginRegistrant.register(with: self.vnptSmartCASDK?.flutterEngine as! FlutterPluginRegistry);
+        
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    // Lấy thông tin về AccessToken & CredentialId
+    func getAuthentication() {
+        self.vnptSmartCASDK?.getAuthentication(callback: { result in
+            self.channelFlutter?.invokeMethod("getAuthenticationResult", arguments: result.toJson())
+        });
+    }
+    
+    func getMainInfo() {
+        self.vnptSmartCASDK?.getMainInfo(callback: { result in
+            self.channelFlutter?.invokeMethod("getMainInfoResult", arguments: result.toJson())
+        })
+    }
+    
+    // Khách hàng xác nhận / hủy giao dịch.
+    func getWaitingTransaction(transactionId: String) {
+        self.vnptSmartCASDK?.getWaitingTransaction(tranId: transactionId, accessToken: "", callback: { result in
+            self.channelFlutter?.invokeMethod("getWaitingTransactionResult", arguments: result.toJson())
+        })
+    }
+    
+    func signOut() {
+        self.vnptSmartCASDK?.signOut(callback: { result in
+            self.channelFlutter?.invokeMethod("signOutResult", arguments: result.toJson())
+        })
+    }
+    
+    func createAccount() {
+        self.vnptSmartCASDK?.createAccount(callback: { result in
+            self.channelFlutter?.invokeMethod("createAccountResult", arguments: result.toJson())
+        })
+    }
+}
